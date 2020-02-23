25634783fd52e4bbc77c7465fce3744609f24aee
Dania Abdul <43537934+Dania-abdul@users.noreply.github.com>
2020-02-23 20:19:33 +0100

    Updates

---
+++ precache-manifest.9f78c1ae4846883d9d97bddf0702f538.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6417bb806713da970150284502c82bef",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6d46256ec66be031d463",
+    "url": "/portfolio/static/css/main.12904769.chunk.css"
+  },
+  {
+    "revision": "231e5ccd9a85617e391d",
+    "url": "/portfolio/static/js/2.f54b4473.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/portfolio/static/js/2.f54b4473.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6d46256ec66be031d463",
+    "url": "/portfolio/static/js/main.4d99a770.chunk.js"
+  },
+  {
+    "revision": "f2b666a286fb3b518acf",
+    "url": "/portfolio/static/js/runtime-main.82ec84df.js"
+  },
+  {
+    "revision": "0d08163da4b8b79d14638ed8c51f4a58",
+    "url": "/portfolio/static/media/img-1.0d08163d.png"
+  },
+  {
+    "revision": "693d5c25a80e7a3349beb1dffe9a6526",
+    "url": "/portfolio/static/media/img-2.693d5c25.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8220c0c66c8f8fb62c9dba3c1b19e7f0.js"
+  "/portfolio/precache-manifest.9f78c1ae4846883d9d97bddf0702f538.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
