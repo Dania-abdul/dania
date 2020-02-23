43bb2c408eec1ca40c31c79d41a460acf2d65412
Dania Abdul <43537934+Dania-abdul@users.noreply.github.com>
2020-02-23 18:03:03 +0100

    Updates

---
+++ precache-manifest.e6015d0ccb6d6cc46f7648d4b0deadb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98d58ba6abe42bd81ca6120900ef0b91",
+    "url": "/dania.github.io/index.html"
+  },
+  {
+    "revision": "819e4082ff27ab218472",
+    "url": "/dania.github.io/static/css/main.12904769.chunk.css"
+  },
+  {
+    "revision": "bfd8de4146d1fe70270d",
+    "url": "/dania.github.io/static/js/2.daaa3a4e.chunk.js"
+  },
+  {
+    "revision": "1af6562b6d659632968c2e5e85a5b301",
+    "url": "/dania.github.io/static/js/2.daaa3a4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "819e4082ff27ab218472",
+    "url": "/dania.github.io/static/js/main.f5e80387.chunk.js"
+  },
+  {
+    "revision": "01c962fa75afecdbf4ea",
+    "url": "/dania.github.io/static/js/runtime-main.4b1f7dbf.js"
+  },
+  {
+    "revision": "0d08163da4b8b79d14638ed8c51f4a58",
+    "url": "/dania.github.io/static/media/img-1.0d08163d.png"
+  },
+  {
+    "revision": "693d5c25a80e7a3349beb1dffe9a6526",
+    "url": "/dania.github.io/static/media/img-2.693d5c25.jpg"
+  }
+]);
