fe8cbcb1d7e2d1fa8e4b4597b9996944e8da277b
Dania Abdul <43537934+Dania-abdul@users.noreply.github.com>
2020-03-27 16:14:27 +0100

    EDIT

---
+++ portfolio/src/home.tsx
@@ -0,0 +1,98 @@
+ import * as React from 'react';
+import { type } from 'os';
+import i18next from 'i18next';
+import Image from './assets/images/myPhoto_1.jpg'
+
+export type HomeState = {
+    counter : number
+}
+
+export type HomeProps = {
+}
+
+export class HomeComponent extends React.Component <HomeProps, HomeState> {
+    constructor(props : HomeProps) {
+        super(props);
+        this.state = {
+            counter : 0
+        }
+    }
+    increase_counter(){
+        this.setState(s_old => ({...s_old, counter : s_old.counter + 1}))
+        // this.setState({...this.state, counter : this.state.counter + 1})
+    }
+    render(){
+        return <>
+                <div className="home-page">
+                    <div className="home-page__inner">
+                        <div className="home-text home-container-m">
+                            <div className="home-text__inner row container-fluid">
+                                <div className="col-md-4">
+                                    <h3 className="home-text__title"> {i18next.t("_home-text__title")}</h3>
+                                </div>
+                                <div className="col-md-8">
+                                    <p className="home-text__text">
+                                        {i18next.t("_home-text__text")} 
+                                    </p>
+                                    <a href="./blog" className="home-text__link">{i18next.t("_home-text__link")}</a>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="code-block home-container-m">
+                            <div className="code-block__inner">
+                                <div className="code-line">
+                                    <div className="comment">{'<About/>'}</div>
+                                </div>
+                                <div className="code-line">
+                                    <p className="method">class</p> 
+                                    <span className="propname"> Dania Abdul</span> 
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">..</div>
+                                    <div className="comment">//  frontend developer  </div>
+                                </div>
+                                {/* <div className="code-line">
+                                    <div className="dots">..</div>
+                                    <div className="comment">// able to write great (S)CSS/HTML </div>
+                                </div> */}
+                                <div className="code-line">
+                                    <div className="dots">..</div>
+                                    <div className="method">constructor</div><p className="symbol">() {'{'}</p>
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">....</div>
+                                    <div className="scope">this</div><p className="symbol">.</p><span className="prop2">name</span><span className="scope"> =</span><span className="string"> ' Dania Abdul '</span>
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">····</div>
+                                    <div className="scope">this</div><p className="symbol">.</p><span className="prop2">dayOfBirthTimestamp</span><span className="scope"> =</span><span className="number"> 29081991</span>
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">····</div>
+                                    <div className="scope">this</div><p className="symbol">.</p><span className="prop2">email</span><span className="scope"> =</span><span className="number"> ' dbashayreh@gmail.com '</span>
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">..</div>
+                                    <p className="symbol">{'}'}</p> 
+                                </div>
+                                <div className="code-line">
+                                     <div className="dots">..</div>
+                                     <span className="propname"> skills</span><p className="symbol">() {'{'}</p>
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">····</div>
+                                    <p className="method">return</p><p className="symbol"> [</p> <span className="string"> 'HTML5/CSS4/JS' , 'Bootstrap/Material Design' , 'Webpack/Gulp' , 'npm/yarn' , 'Github/gitlab' , 'W3C/ARIA/WCAG' , 'Wordpress ', 'Photoshop/Illustrator', 'Sketch/Figma' , 'UX/UI', 'Email templates'</span> 
+                                    <p className="symbol"> ]</p>
+                                    
+                                </div>
+                                <div className="code-line">
+                                    <div className="dots">..</div>
+                                    <p className="symbol"> {'}'}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </>
+    }
+}
